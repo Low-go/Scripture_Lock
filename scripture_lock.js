5984c983a55818d524fa0e290c48ae4bedcaf9e7
Low-go <lorranalvesgaldino98@gmail.com>
2025-04-04 11:34:16 -1000

    attempt at redirecting to scripture site

--- scripture_lock.js
+++ scripture_lock.js
@@ -1,1 +1,12 @@
-document.body.style.border = "5px solid red";
+redirect_url = "https://www.churchofjesuschrist.org/study/scriptures/bofm"
+
+browser.webRequest.onBeforeRequest.addListener(
+    function(details){
+        if (details.url.startsWith(redirect_url)){
+            return {}; // allow scripture page
+        }
+        return { redirect_url }; // force scripture page
+    },
+    { urls: ["<all_urls"] },
+    ["blocking"]
+);
